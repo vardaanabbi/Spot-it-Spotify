0abbe2a8fe40a367ed402cb956d8f25ac44b14f9
Vardaan <vardaanabbi18@gmail.com>
2026-08-29 16:48:43 +0530

    85% completed with dynamic playlists

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,7 @@
 let currentSong = new Audio() 
 let songList ; 
 let currFolder ; 
+let saved_vol ;
 
 function secondsToMinutesSeconds(seconds) {
     if (isNaN(seconds) || seconds < 0) {
@@ -33,6 +34,7 @@ async function getSongs(folder) {
     } 
 
     let songJS = document.querySelector(".songList").getElementsByTagName("ol")[0];
+    songJS.innerHTML = "" ; 
     for (const song of songList) {
         songJS.innerHTML += `<li 
     class="flex gap-3 py-3 border-white border rounded-sm my-3 justify-between">
@@ -72,10 +74,65 @@ document.querySelector(".sName").innerHTML = track
 document.querySelector(".songTime").innerHTML = "00 : 00"
 }
 
+async function displayAlbums() {
+    let a = await fetch("http://127.0.0.1:5500/src/songs/") ; 
+    let response = await a.text() ; 
+    let element = document.createElement("div") ; 
+    element.innerHTML = response ; 
+    let anchors = element.getElementsByTagName("a") ;
+    let array = Array.from(anchors) 
+
+    for(let i = 0 ; i<array.length ; i ++)
+    {
+        const e = array[i] ; 
+        if(e.href.includes("/songs/"))
+        {
+            let card_container = document.querySelector(".card_container")
+            let folder = (e.href.split("/").splice(-1)[0]) ; 
+            // get he meta data of the folder 
+            console.log(folder)
+            let a = await fetch(`http://127.0.0.1:5500/src/songs/${folder}/info.json`)
+            let response = await a.json() ; 
+            console.log(response)
+            
+            
+            card_container.innerHTML += `<div data-folder="${folder}" class="card w-60.5 p-2.5 bg-[#252525] relative group max-[520px]:w-[96vw] hover:bg-[rgb(54,50,50)] hover:cursor-pointer transition-all duration-600">
+
+                            <div class="play w-7 h-7 bg-[#1fdf64] p-1 flex rounded-[50%] justify-center top-50 right-4 absolute opacity-0 transition-all ease-out duration-500 group-hover:opacity-100 group-hover:top-46 ">
+                                <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                <path d="M5 4V21L19 12L5 4Z"
+                                stroke="#141B34" fill="#000" stroke-width="1.5"
+                                stroke-linejoin="round" />
+                                </svg>
+                            </div>
+
+                            <img class="object-contain w-full" src="/src/songs/${folder}/cover.jpeg" alt="">
+                            <h2 class="font-bold">${response.title}</h2>
+                            <p>${response.description}</p>
+                        </div>`
+    }    
+}
+
+// load the playlist whenever card is clicked
+
+// one important distinction between target and currentTarget ; target : agar kahi click kr rhe ho to jaha click kiya vaha ki info , curratasrget me if you click somewhere and that s inside your target where you have applied event listener then currenttarget me vahi element ki info milegi 
+
+Array.from(document.getElementsByClassName("card")).forEach((e)=>{e.addEventListener("click", async (item)=>{
+    console.log("fetching songs") ; 
+    songList = await getSongs(`songs/${item.currentTarget.dataset.folder}`)
+    playMusic(songList[0])
+}) })
+}
+
     async function main(){
 
     await getSongs("songs/punjabi") ; 
     playMusic(songList[0] , true) ; 
+
+    // display all the albums on the page :
+     await displayAlbums() ; 
+
 
     // attach event listener to play buttons :
 
@@ -135,12 +192,37 @@ nextbtn.addEventListener("click" , ()=> {let index = songList.indexOf(currentSon
 
 // adding event listener to volume range  : 
 
-range.addEventListener("input" , (e)=>{currentSong.volume = parseInt((e.target.value))/100 })
+range.addEventListener("input" , (e)=>{currentSong.volume = parseInt((e.target.value))/100 ;
+    saved_vol = currentSong.volume 
+})
 
-// load the playlist whenever card is clicked
+// add event listener to mute button ;
 
-// one important distinction between target and currentTarget ; target : agar kahi click kr rhe ho to jaha click kiya vaha ki info , curratasrget me if you click somewhere and that s inside your target where you have applied event listener then currenttarget me vahi element ki info milegi 
-Array.from(document.getElementsByClassName("card")).forEach((e)=>{e.addEventListener("click", (item)=>{console.log(item.currentTarget.dataset.folder)}) })
+document.querySelector(".volume > img").addEventListener("click" , (e)=>{
+    if(e.target.src.includes("volume.svg"))
+    {
+        e.target.src = e.target.src.replace("volume.svg" , "mute.svg") ; 
+        range.value = 0 
+        currentSong.volume = 0 ;
+    }
+
+    else
+    {
+        e.target.src = e.target.src.replace("mute.svg" , "volume.svg") ; 
+        if(!saved_vol)
+        {
+            currentSong.volume = 0.36 ; 
+            range.value = 36 ; 
+        }
+
+        else
+        {
+            currentSong.volume = saved_vol ;
+            range.value = saved_vol*100
+        }
+        
+    }
+})
 
 }
 
